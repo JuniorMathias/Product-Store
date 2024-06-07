fcd87fabc1adf9638d08f132891d5fb86110c0a8
junior <juniormathiias@gmail.com>
2024-06-06 22:38:41 -0300

    gave style to form

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
@@ -16,7 +17,7 @@ export class CreateComponent {
       nonNullable: true, validators: Validators.required
     }),
   });
-  onSubmit(){
+  onSubmit() {
     this.form.controls.title.value;
   }
 }
